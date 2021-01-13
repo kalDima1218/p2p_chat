770577f1869af75e5cef7f5e9831562d10d71df8
Dmitry Kalugin <47830103+kalDima1218@users.noreply.github.com>
2021-01-13 18:06:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,30 +3,25 @@ import pickle
 
 s = socket.socket()
 
-host = '46.229.212.108'
+host = 'SE.RV.ER.IP'
 port = 54320
 
 s.bind((host, port)) 
 s.listen(10)
 
-users = []
-hosts = []
-
-def find_user(user):
-    for x,i in enumerate(users):
-        if str(i) == str(user):
-            return hosts[x]
+users = {}
 
 while True: 
     c, addr = s.accept()
     print ('Got connection from', addr) 
     mes = pickle.loads(c.recv(1024))
     if str(mes[1]) == "reg":
-        users.append(mes[0])
-        hosts.append((addr[0], int(mes[2])))
-    if str(mes[1]) == "get":
-        print(find_user(mes[0]))
-        c.send(pickle.dumps(find_user(mes[0])))
-    print("Connection closed")
-    
+        users[mes[0]] = (addr[0], int(mes[2]))
+    elif str(mes[1]) == "get":
+        try:
+            c.send(pickle.dumps(users[mes[0]]))
+        except:
+            pass
+    elif mes == "stun":
+        c.send(addr[0].encode())
     c.close()
