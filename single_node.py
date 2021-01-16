934db4209f1aa4e87c344aee16e3d2f33e969172
Dmitry Kalugin <47830103+kalDima1218@users.noreply.github.com>
2021-01-16 17:52:38 +0300

    Update single_node.py

--- single_node.py
+++ single_node.py
@@ -31,7 +31,7 @@ def redirect (ip, in_port, out_port):
 		name = data.decode()
 	
 		s = socket.socket() 
-		s.connect(('46.229.212.108', 54320))
+		s.connect(('IP.AD.RE.SS', 54320))
 		s.send(pickle.dumps((name,"reg", str(out_port))))
 		s.close()
 	
@@ -59,7 +59,7 @@ def redirect (ip, in_port, out_port):
 	s2.close()
 
 s = socket.socket() 
-s.connect(('46.229.212.108', 54320))
+s.connect(('IP.AD.RE.SS', 54320))
 s.send(pickle.dumps(("stun")))
 data, addr = s.recvfrom(1024)
 ip = data.decode()
